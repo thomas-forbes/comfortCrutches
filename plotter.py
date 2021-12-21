55d977ae1012cce34e40ffeacaeb6ebf0cdb7534
Folded-Code <thomas.reilly.forbes@gmail.com>
2021-12-21 19:50:25 +0000

    added some functionality and refactored a bit plotter

--- plotter.py
+++ plotter.py
@@ -1,18 +1,22 @@
+from click.decorators import option
 import matplotlib.pyplot as plt
 import numpy as np
+import logging
 import os
-import sys
+from optparse import OptionParser
 from txtToCsv import listify
 
 
-# groups = ['bio', 'chem', 'thomasHouse', 'tuesday', 'vedhBeingAnnoying', 'thursday']
 groups = ['tuesday']
 subSets = ['ours', 'notours']
 
 interval = 0.5
 
-# namesToSubN = {'adrian': 0, 'andrew': 11, 'bree': 2, 'fran': 12, 'hanna': 9, 'issy': 5, 'jack': 6,
-#                'killian': 7, 'sophie': 8, 'thomas': 4, 'tristan': 10, 'vedh': 1, 'will': 3, 'zoe': 13}
+students = []
+for g in groups:
+    students += [x.split('.')[0]
+                 for x in os.listdir(f'./{g}/{subSets[0]}')]
+students.sort()
 
 
 def getStudentData(stu):
@@ -25,6 +29,7 @@ def getStudentData(stu):
                 data = listify(fileN)
                 data = [float(x[1]) for x in data]
 
+                logging.debug(data)
                 out[s] = data
 
                 sData = sorted(data)
@@ -38,13 +43,22 @@ def getStudentData(stu):
     return out
 
 
+def getStudentsData():
+    sData = []
+    for stu in students:
+        stuD = getStudentData(stu)
+        sData.append(stuD)
+    return sData
+
+
+# Plotting funcs
 def plotStu(stu):
     data = getStudentData(stu)
-    # medians = data['medians']
 
     for s in subSets:
         plt.plot(np.arange(0, len(data[s])*interval, interval), data[s])
-    print(data['medians'], '\n', data['median'])
+    logging.info(data['medians'])
+    logging.info(data['median'])
 
     plt.xlabel('Seconds')
     plt.ylabel('Resistance')
@@ -54,29 +68,49 @@ def plotStu(stu):
     plt.ylim(-1, 120)
 
 
+# Plots the medians of each student and a line of the
 def plotMedians():
-    students = []
-    for g in groups:
-        students += [x.split('.')[0]
-                     for x in os.listdir(f'./{g}/{subSets[0]}')]
-    students.sort()
-
-    sData = []
-    for stu in students:
-        stuD = getStudentData(stu)
-        sData.append(stuD)
-
+    sData = getStudentsData()
     sData.sort(key=lambda x: -float('inf')
                if type(x['median']) is str else x['median'], reverse=True)
+
+    meds = []
     for i, s in enumerate(sData):
-        print(s['median'], s['name'])
+        logging.debug(s['median'], s['name'])
         if type(s['median']) is not str:
             plt.plot(i, s['median'], 'ro')
+            meds.append(s['median'])
+    plt.plot(range(len(meds)), [sum(meds) / len(meds)] * len(meds), lw=3)
 
     plt.ylim(-1, 1)
-    # print({x: i for i, x in enumerate(students)})
 
 
-stu = sys.argv[1]
-plotStu(stu)
-plt.show()
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Setup Arg options
+parser = OptionParser()
+parser.add_option('-n', dest='useN', default=False,
+                  action='store_true', help='use newtons')
+parser.add_option('-s', dest='stu', help='Name of student')
+parser.add_option('-m', dest='doMedians', default=False,
+                  action='store_true', help='Plot medians instead')
+parser.add_option('-P', dest='doPlot', default=True,
+                  action='store_false', help='Do not show plot')
+
+# Check options
+(options, args) = parser.parse_args()
+stu = options.stu
+USE_NEWTONS = options.useN
+
+# Run funcs
+if options.doMedians:
+    plotMedians()
+elif stu in students:
+    plotStu(stu)
+else:
+    parser.print_help()
+
+
+if options.doPlot:
+    plt.show()
