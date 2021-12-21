53f845ac2d8a7f536ce868d77e2b7a1ae26d6eee
Folded-Code <thomas.reilly.forbes@gmail.com>
2021-12-21 10:43:42 +0000

    got more data from tuesday

--- plotter.py
+++ plotter.py
@@ -6,7 +6,7 @@ from txtToCsv import listify
 
 
 # groups = ['bio', 'chem', 'thomasHouse', 'tuesday', 'vedhBeingAnnoying', 'thursday']
-groups = ['monday3']
+groups = ['tuesday']
 subSets = ['ours', 'notours']
 
 interval = 0.5
