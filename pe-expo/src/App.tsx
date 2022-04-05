3f4c8aa42e30203c7f161780e5d12d7ba59ffc5a
Folded-Code <thomas.reilly.forbes@gmail.com>
2022-04-05 11:06:34 +0100

    finished shit

--- pe-expo/src/App.tsx
+++ pe-expo/src/App.tsx
@@ -10,16 +10,7 @@ import {
   Autocomplete,
   TextField,
 } from '@mui/material'
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts'
+import { CanvasJSChart } from 'canvasjs-react-charts'
 
 const theme = createTheme({
   palette: {
@@ -30,6 +21,26 @@ export default function App() {
   const [subList, setSubList] = useState([])
   const [subject, setSubject] = useState('')
   const [subData, setSubData]: [any[], any] = useState([])
+
+  const [data, setData]: [any, any] = useState([])
+
+  const updateSubData = (stu) => {
+    let out: any[] = []
+
+    out.push({
+      name: 'ours',
+      type: 'spline',
+      showInLegend: true,
+      dataPoints: stu.ours.map((x, i) => ({ y: x, x: i })),
+    })
+    out.push({
+      name: 'notours',
+      type: 'spline',
+      showInLegend: true,
+      dataPoints: stu.notours.map((x, i) => ({ y: x, x: i })),
+    })
+    return out
+  }
   useEffect(() => {
     fetch('http://localhost:3001/data')
       .then((resp) => resp.json())
@@ -37,25 +48,14 @@ export default function App() {
         let tempSublist = data.map((x: any) => x.name)
         setSubList(tempSublist)
         setSubject(tempSublist[0])
-        let out: any[] = []
-        Object.keys(data[0].ours).forEach((x, i) => {
-          out.push({ ours: data[0].ours[x], name: x })
-        })
-        Object.keys(data[0].notours).forEach((x, i) => {
-          if (out[i]) {
-            out[i].notours = data[0].notours[x]
-          } else {
-            out.push({ ours: data[0].notours[x], name: x })
-          }
-        })
-        console.log(out)
-        setSubData(out)
+        setSubData(updateSubData(data[0]))
+        setData(data)
       })
   }, [])
 
   useEffect(() => {
-    console.log(subData)
-  }, [setSubData])
+    if (subject) setSubData(updateSubData(data.find((x) => x.name == subject)))
+  }, [subject])
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -83,13 +83,49 @@ export default function App() {
               />
             )}
           />
-          <LineChart width={800} height={300} data={subData}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            {/* <CartesianGrid stroke="#eee" strokeDasharray="20 20" /> */}
-            <Line type="monotone" dataKey="ours" stroke="#8884d8" />
-            <Line type="monotone" dataKey="notours" stroke="#82ca9d" />
-          </LineChart>
+          <CanvasJSChart
+            options={{
+              theme: 'dark2',
+              title: {
+                text: subject,
+              },
+              axisY: {
+                title: 'Pressure (KPa)',
+              },
+              axisX: {
+                title: 'Time (s)',
+              },
+              data: subData,
+              // data: [
+              //   {
+              //     type: 'line',
+              //     // xValueFormatString: 'MMM YYYY',
+              //     // yValueFormatString: '$#,##0.00',
+              //     dataPoints: [{ x: 1, y: 2 }],
+              //   },
+              // ],
+            }}
+          />
+          {/* <LineChart width={800} height={300} data={subData}>
+          <XAxis
+            dataKey="name"
+            label={{
+              value: 'Time (s)',
+              // offset: -1,
+              position: 'bottom',
+            }}
+          />
+          <YAxis
+          // label={{
+          //   value: 'Pressure (KPa)',
+          //   angle: -90,
+          //   position: 'outsideLeft',
+          // }}
+          />
+          <Legend verticalAlign="top" height={36} />
+          <Line type="monotone" dataKey="ours" stroke="#8884d8" />
+          <Line type="monotone" dataKey="notours" stroke="#82ca9d" />
+        </LineChart> */}
         </Stack>
       </Container>
     </ThemeProvider>
